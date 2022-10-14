b1e95d707b6b31fdc4f6084dfaad842d96965000
kiyo1234567890 <2101192@s.asojuku.ac.jp>
2022-10-14 12:23:06 +0900

    Create test.php
    なんかつくった

---
+++ test.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+<title>ログイン画面</title>
+<style>
+html {
+	height: 100%;
+	margin: 0 auto;
+	padding: 0;
+	display: table;
+}
+body {
+	min-height: 100%;
+	margin: 0 auto;
+	padding: 0;
+	display: table-cell;
+	vertical-align: middle;
+}
+</style>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+</head>
+
+<body>
+<form class="text-center card">
+<div class="card-body">
+<br><br><br>
+<h1 class="card-title">ログイン</h1>
+<br>
+
+<form>
+<div class="text-center">
+<p class="card-text"><input type="id" name="user_id" style="width:450px;" class="form-control alert-primary" placeholder="メールアドレスを入力してください"><br>
+<p class="card-text"><input type="id" name="user_id" style="width:450px;" class="form-control alert-primary" placeholder="パスワードを入力してください">
+</div>
+</form>
+<br><br><br><br><br>
+<button type="buttton" style="width:400px;" class="btn btn-outline-primary text-center">ログイン</button>
+</div>
+</div>
+</form>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+</body>
+
+</html>
